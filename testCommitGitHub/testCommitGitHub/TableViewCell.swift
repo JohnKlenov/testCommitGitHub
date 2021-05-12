52401e8836cc87a7fe93515029331dfcae2a92ed
JohnKlenov <klenovptz@mail.ru>
2021-05-12 16:49:26 +0300

    added XibTableViewCell

---
+++ testCommitGitHub/testCommitGitHub/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  testCommitGitHub
+//
+//  Created by Evgenyi on 5/12/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLable: UILabel!
+    
+    @IBOutlet weak var bodyLable: UILabel!
+    
+}
