455586e3ee6bab7062d6d089583c1483455cbfd4
JohnKlenov <klenovptz@mail.ru>
2021-05-14 14:16:26 +0300

    added enum HTTPMethod + APIs

---
+++ testCommitGitHub/testCommitGitHub/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  testCommitGitHub
+//
+//  Created by Evgenyi on 5/14/21.
+//
+
+import Foundation
+
+
+enum HTTPMethod:String {
+    case POST
+    case GET
+    case DELETE
+    case PUT
+}
+
+enum APIs:String {
+    case posts
+    case users
+    case comments
+}
+
+
+
