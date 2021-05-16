56f19ce006aa45fe6fde21b28a1929a5977e1226
JohnKlenov <klenovptz@mail.ru>
2021-05-16 18:33:50 +0300

    added init post + present alert

--- testCommitGitHub/testCommitGitHub/ViewController.swift
+++ testCommitGitHub/testCommitGitHub/ViewController.swift
@@ -30,14 +30,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         
-        objectNM.getAllPosts { (post) in
+        objectNM.getAllPosts { [weak self] (post) in
             DispatchQueue.main.async {
-                self.massivePost = post
+                self?.massivePost = post
             }
         }
     }
 
     @IBAction func createPost(_ sender: Any) {
+        let post = Post(userId: 333, title: "Hello world", body: "Swift is best language")
+        objectNM.postCreate(post: post) { (postServer) in
+            post.id = postServer.id
+            print(post.id)
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "WoW!", message: "Your post was create", preferredStyle: .alert)
+                self.present(alert, animated: true, completion: nil)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    alert.dismiss(animated: true, completion: nil)
+                }
+                
+            }
+            
+        }
         
     }
     
